319f0d66a8cf5c901a647de92399447f51fca6b1
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-03 00:44:59 +0700

    edit header

--- inc_footer.php
+++ inc_footer.php
@@ -3,6 +3,7 @@
 </footer>
 </section>
 <script type="text/javascript" src="http://<?php echo $url;?>js/jquery_1.9.1.js"></script>
+<script type="text/javascript" src="http://<?php echo $url;?>js/jquery_bootstrap.min.js"></script>
 <script type="text/javascript" src="http://<?php echo $url;?>js/jquery_tabs.js"></script>
 <script type="text/javascript" src="http://<?php echo $url;?>js/jquery_scrollTo.js"></script>
 <script type="text/javascript" src="http://<?php echo $url;?>js/jquery_function.js"></script>
--- inc_header.php
+++ inc_header.php
@@ -6,6 +6,8 @@ $url = $_SERVER['SERVER_NAME'] . "/alumni/";
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="keyword" content=""/>
         <meta name="description" content=""/>
         <meta name="google-site-verification" content=""/>
@@ -23,24 +25,38 @@ $url = $_SERVER['SERVER_NAME'] . "/alumni/";
     </head>
     <body>
         <section id="container">
-            <header id="header"class="">
-                <div class="header_top wrapper_12"></div>
-                <div class="header_bottom wrapper_12">
-                    <?php if($page == "page1") {echo '
-                        <ul id="profileMenu">
-                            <li><a href="http://'.$url.'">Home</a></li>
-                            <li><a href="http://'.$url.'">Profile</a></li>
-                            <li><a href="http://'.$url.'">Alumni Networks</a></li>
-                            <li><a href="http://'.$url.'">Post</a></li>
-                            <li><a href="http://'.$url.'">Groups</a></li>
-                            <li><a href="http://'.$url.'">Promotions Area</a></li>
-                        </ul>
-                        <!--end profileMenu-->
-                        <form class="hm_search search" action="" method="">
-                            <input type="text" placeholder="Search" />
-                            <input type="image" src="http://'.$url.'images/material/searchButton.png" />
-                        </form>';}
-                    ?>
+            <header id="header"class="navbar navbar-inverse">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".header_bottom">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Brand</a>
+                    </div>
+                    <div class="header_top"></div>
+                    <div class="collapse navbar-collapse header_bottom">
+                        <?php if($page == "page1") {echo '
+                            <ul id="profileMenu" class="nav navbar-nav">
+                                <li><a href="http://'.$url.'">Home</a></li>
+                                <li><a href="http://'.$url.'">Profile</a></li>
+                                <li><a href="http://'.$url.'">Alumni Networks</a></li>
+                                <li><a href="http://'.$url.'">Post</a></li>
+                                <li><a href="http://'.$url.'">Groups</a></li>
+                                <li><a href="http://'.$url.'">Promotions Area</a></li>
+                            </ul>
+                            <!--end profileMenu-->
+                            <form class="hm_search search navbar-form navbar-left" action="" method="" role="search">
+                                <div class="form-group">
+                                    <input class="form-control" type="text" placeholder="Search" />
+                                    <input type="image" src="http://'.$url.'images/material/searchButton.png" />
+                                </div>
+                            </form>';}
+                        ?>
+                    </div>
                 </div>
+                
             </header>
     
