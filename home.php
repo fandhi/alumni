38e664a84142f2096dfab75aa1851f1e9e0ac360
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-23 15:11:36 +0700

    -merapikan tampilan home untuk tampilan tablet kebawah -edit css

--- home.php
+++ home.php
@@ -5,30 +5,30 @@ include 'inc_header.php';
 <section id="middle">
     <div class="container">
         <div class="row">
-            <div id="profile_sett" class="col-lg-3">
+            <div id="profile_sett" class="col-sm-4 col-md-3 col-lg-3">
                 <div class="well">
-                <a class="img" href="profile.php">
-                    <img class="img-thumbnail" src="http://<?php echo $url ?>images/content/profile-1.jpg" alt="" />
-                </a>
-                <ul class="menu_profile list-group">
-                    <li><a class="list-group-item" href="inbox.php">Inbox<span class="badge pull-right">42</span></a></li>
-                    <li><a class="list-group-item" href="notifications.phpd">Notifications<span class="badge pull-right">18</span></a></li>
-                    <li><a class="list-group-item" href="follower.php">Follower<span class="badge pull-right">4382</span></a></li>
-                    <li><a class="list-group-item" href="follow.php">Follow<span class="badge pull-right">24</span></a></li>
-                    <li><a class="list-group-item" href="group.php">Group</a></li>
-                    <li><a class="list-group-item" href="http://<?php echo $url ?>settings.php">Settings</a></li>
-                </ul>
+                    <a class="img" href="profile.php">
+                        <img class="img-thumbnail" src="http://<?php echo $url ?>images/content/profile-1.jpg" alt="" />
+                    </a>
+                    <ul class="menu_profile list-group">
+                        <li><a class="list-group-item ajax-menu" href="inbox.php">Inbox<span class="badge pull-right">42</span></a></li>
+                        <li><a class="list-group-item ajax-menu" href="notifications.phpd">Notifications<span class="badge pull-right">18</span></a></li>
+                        <li><a class="list-group-item ajax-menu" href="follower.php">Follower<span class="badge pull-right">4382</span></a></li>
+                        <li><a class="list-group-item ajax-menu" href="follow.php">Follow<span class="badge pull-right">24</span></a></li>
+                        <li><a class="list-group-item ajax-menu" href="group.php">Group</a></li>
+                        <li><a class="list-group-item" href="http://<?php echo $url ?>mysettings-profilesetting.php">Settings</a></li>
+                    </ul>
                 </div>
             </div>
 
-        <!--end profile_sett-->
-            <div id="newsFeed" class="col-lg-6">
+            <!--end profile_sett-->
+            <div id="newsFeed" class="col-sm-8 col-md-6 col-lg-6">
                 <div class="nf_title">News Feed</div>
                 <div class="nf_list well">
                     <form class="form-horizontal" action="" method="" role="form">
                         <div class="row">
                             <div class="col-lg-12">
-                        <textarea class="form-control" placeholder="Whats going on + REPLY"></textarea>
+                                <textarea class="form-control" placeholder="Whats going on + REPLY"></textarea>
                             </div>
                         </div>
                     </form>
@@ -40,12 +40,14 @@ include 'inc_header.php';
             </div>
             <div id="ajax-status"></div>
 
-        <!--end newsFeed-->
-            <div id="trendPost" class="col-lg-3 omega">
-                <form class="form-inline tp_search search" action="" method="" role="form">
+            <!--end newsFeed-->
+            <div id="trendPost" class="col-sm-12 col-md-3 col-lg-3 omega">
+                <form class="form tp_search search" action="" method="" role="form">
                     <div class="form-group">
-                        <input class="form-control" type="text" placeholder="Search" />
-                    <input type="image" src="http://<?php echo $url ?>images/material/searchButton.png" />
+                        <div class="col-xs-12 col-sm-12 col-md-12 prefix-0">
+                            <input class="form-control" type="text" placeholder="Search" />
+                        </div>
+                        <input type="image" src="http://<?php echo $url ?>images/material/searchButton.png" />
                     </div>
                 </form>
                 <div class="trend_post panel panel-primary">
@@ -58,11 +60,11 @@ include 'inc_header.php';
                         <div class="list_trend"></div>
                         <div class="list_trend"></div>
                     </div>
-                    
+
                 </div>
             </div><!--end trendPost-->
         </div><!--/ row -->
-        
+
         <div class="row">
             <div id="sugest" class="col-lg-12">
                 <ul class="mytabs" id="tabs">
@@ -75,7 +77,7 @@ include 'inc_header.php';
             </div>
         </div>
     </div>
-    <!--end wrapper_12-->
+    <!--end container-->
 </section>
 <!--end middle-->
 <?php include 'inc_footer.php'; ?>
