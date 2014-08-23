f79d0e7bc2962864bed96550b048a61659ab9330
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-23 23:07:04 +0700

    - view img thumnail jd lebih responsive

--- people-you-may-know.php
+++ people-you-may-know.php
@@ -1,54 +1,54 @@
 <ul class="list_people">
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
     </li>
-    <li class="col-lg-1">
-        <a class="img">
+    <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+        <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
             <span class="nm_profile">Name</span>
         </a>
