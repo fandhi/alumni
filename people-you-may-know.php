deb0cad44603cb99c2c2a1f31782f4ad2d746738
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-31 22:38:03 +0700

    - penambhan konten fakultas, link follow

--- people-you-may-know.php
+++ people-you-may-know.php
@@ -2,55 +2,92 @@
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
     <li class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a class="img" href="">
             <img class="img-thumbnail" src="images/content/profile-1.jpg" alt="" />
-            <span class="nm_profile">Name</span>
+            <span class="lp-detail-intro">
+                <span class="nm_profile">Name</span>
+                <small class="text-success">Kedokteran - 2010</small>
+            </span>
         </a>
+        <a class="text-info" href=""><small>Follow</small></a>
     </li>
 </ul>
+<button class="btn btn-primary" type="button">Reload More</button>
