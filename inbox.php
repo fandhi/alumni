a0ea78304d2cdf79361d9cce98663e15e540bb1d
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-10-14 01:19:39 +0700

    edit message.,., tp blom rapi heheh

--- inbox.php
+++ inbox.php
@@ -1,26 +1,73 @@
 <div class="nf_title">Inbox</div>
 <div class=" nf_list well row">
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-4 list-group">
-        <div class="append-bottom-2">
-            <div class="text-primary"><strong>Subject :</strong></div>
-            <h6>Buka Puasa dimana yah?!?!</h6>
-            <div class="text-primary"><strong>Participants :</strong></div>
-            <div>Cris, Ronaldo, Messi, Julio Sesar, Maliuque</div>
-            <div class="text-primary"><strong>Date Sent :</strong></div>
-            <div>Jan, 4 2014</div>
+    <div id="accordionInbox" class="col-xs-12 col-sm-6 col-md-3 col-lg-4 list-groups panel-group">
+        <div class="panel panel-default">
+            <h5 class="panel-title">
+                <a href="#collapse-1" class="list-group-item active" data-toggle="collapse" data-parent="#accordionInbox">Buka Puasa dimana yah?!?!</a>
+            </h5>
+            <div id="collapse-1" class="append-bottom-2e panel-collapse collapse in">
+                <div class="panel-body">
+                    <div class="text-primary"><strong>Subject :</strong></div>
+                    <h6>Buka Puasa dimana yah?!?!</h6>
+                    <div class="text-primary"><strong>Participants :</strong></div>
+                    <div>Cris, Ronaldo, Messi, Julio Sesar, Maliuque</div>
+                    <div class="text-primary"><strong>Date Sent :</strong></div>
+                    <div>Jan, 4 2014</div>
+                </div>
+            </div>
         </div>
-        <a href="" class="list-group-item active">
-            <h5 class="list-group-item-heading">Halo bos, somse amat apa kabar</h5>
-        </a>
-        <a href="" class="list-group-item">
-            <h5 class="list-group-item-heading">Halo bos, somse amat apa kabar</h5>
-        </a>
-        <a href="" class="list-group-item">
-            <h5 class="list-group-item-heading">Halo bos, somse amat apa kabar</h5>
-        </a>
-        <a href="" class="list-group-item">
-            <h5 class="list-group-item-heading">Halo bos, somse amat apa kabar</h5>
-        </a>
+        <div class="panel panel-default">
+            <h5 class="panel-title"><a href="#collapse-2" class="list-group-item" data-toggle="collapse" data-parent="#accordionInbox">Halo bos, somse amat apa kabar</a></h5>
+            <div id="collapse-2" class="append-bottom-2e panel-collapse collapse">
+                <div class="panel-body">
+                    <div class="text-primary"><strong>Subject :</strong></div>
+                    <h6>Buka Puasa dimana yah?!?!</h6>
+                    <div class="text-primary"><strong>Participants :</strong></div>
+                    <div>Cris, Ronaldo, Messi, Julio Sesar, Maliuque</div>
+                    <div class="text-primary"><strong>Date Sent :</strong></div>
+                    <div>Jan, 4 2014</div>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <h5 class="panel-title"><a href="#collapse-3" class="list-group-item" data-toggle="collapse" data-parent="#accordionInbox">Halo bos, somse amat apa kabar</a></h5>
+            <div id="collapse-3" class="append-bottom-2e panel-collapse collapse">
+                <div class="panel-body">
+                    <div class="text-primary"><strong>Subject :</strong></div>
+                    <h6>Buka Puasa dimana yah?!?!</h6>
+                    <div class="text-primary"><strong>Participants :</strong></div>
+                    <div>Cris, Ronaldo, Messi, Julio Sesar, Maliuque</div>
+                    <div class="text-primary"><strong>Date Sent :</strong></div>
+                    <div>Jan, 4 2014</div>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <h5 class="panel-title"><a href="#collapse-4" class="list-group-item" data-toggle="collapse" data-parent="#accordionInbox">Halo bos, somse amat apa kabar</a></h5>
+            <div id="collapse-4" class="append-bottom-2e panel-collapse collapse">
+                <div class="panel-body">
+                    <div class="text-primary"><strong>Subject :</strong></div>
+                    <h6>Buka Puasa dimana yah?!?!</h6>
+                    <div class="text-primary"><strong>Participants :</strong></div>
+                    <div>Cris, Ronaldo, Messi, Julio Sesar, Maliuque</div>
+                    <div class="text-primary"><strong>Date Sent :</strong></div>
+                    <div>Jan, 4 2014</div>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <h5 class="panel-title"><a href="#collapse-5" class="list-group-item" data-toggle="collapse" data-parent="#accordionInbox">Halo bos, somse amat apa kabar</a></h5>
+            <div id="collapse-5" class="append-bottom-2e panel-collapse collapse">
+                <div class="panel-body">
+                    <div class="text-primary"><strong>Subject :</strong></div>
+                    <h6>Buka Puasa dimana yah?!?!</h6>
+                    <div class="text-primary"><strong>Participants :</strong></div>
+                    <div>Cris, Ronaldo, Messi, Julio Sesar, Maliuque</div>
+                    <div class="text-primary"><strong>Date Sent :</strong></div>
+                    <div>Jan, 4 2014</div>
+                </div>
+            </div>
+        </div>
         <button class="btn btn-primary append-top-2" type="button">More Message</button>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-9 col-lg-8">
